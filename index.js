1772f943bf2aae3a8a7a5570ab1c3bea587f62f0
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-06-09 18:51:20 +0800

    added inital server setup

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+// Configure express app
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// app.use(express.static('public'))
+app.set("view engine", "ejs");
+
+// Top-level middleware
+
+// Main routes
+app.get("/", (req, res) => {
+    res.render("index", {
+        currentTime: new Date().toString()
+    });
+})
+
+// Register routers
+app.use("/misc", require("./routes/misc"));
+
+// Start server
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Server is running on port ${process.env.SERVER_PORT}`)
+})
+
+// Server initialisation with sequelize
+// const db = require("./models");
+// db.sequelize.sync({ alter: true })
+//     .then(() => {
+//         app.listen(process.env.SERVER_PORT, () => {
+//             console.log(`Server is running on port ${process.env.SERVER_PORT}`)
+//         })
+//     })
+//     .catch(err => {
+//         console.error(err)
+//     })
---
+++ services/Logger.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config()
+
+class Logger {
+    // TODO
+}

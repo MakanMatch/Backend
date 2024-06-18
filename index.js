e1751750be4250d24cc56b11227f7d40813265c2
Lincoln Lim <lincolnlim267@gmail.com>
2024-06-18 18:46:22 +0800

    shifted review routes to register routers section

--- index.js
+++ index.js
@@ -34,14 +34,12 @@ app.get("/", (req, res) => {
     });
 })
 
-const reviewsRouter = require("./routes/reviews/reviews");
-
-app.use("/reviews", reviewsRouter);
-
 // Register routers
 app.use("/misc", require("./routes/misc"));
 app.use("/orders", require("./routes/orders/preOrder"));
 app.use("/reviews", require("./routes/reviews/reviews"));
+const reviewsRouter = require("./routes/reviews/reviews");
+app.use("/reviews", reviewsRouter);
 
 // Start server
 if (!SEQUELIZE_ACTIVE) {

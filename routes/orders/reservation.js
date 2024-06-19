27531d20159ba84cb5643d8a5208fd56084d853e
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-06-20 00:19:21 +0800

    added storeFile middleware and uploadListingImage endpoint

--- routes/orders/reservation.js
+++ routes/orders/reservation.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { FoodListing } = require('../../models')
 const Universal = require('../../services/Universal');
 const FileManager = require('../../services/FileManager');
+const { storeFile } = require('../../middleware/storeFile');
 
 router.get("/listingDetails", async (req, res) => {
     const dummyListingID = Universal.data["DUMMY_LISTING_ID"]
@@ -24,6 +25,34 @@ router.get("/img/:name", async (req, res) => {
     }
 
     res.sendFile(filePrep.substring("SUCCESS: File path: ".length))
+})
+
+router.post("/uploadListingImage", async (req, res) => {
+    if (!req.body.listingID) {
+        res.status(400).send("ERROR: Listing ID not provided.")
+    }
+
+    // const listingID = req.listingID
+    const listingID = Universal.data["DUMMY_LISTING_ID"]
+    const listing = await FoodListing.findByPk(listingID)
+    if (!listing) {
+        res.status(404).send("ERROR: Listing not found.")
+        return
+    }
+
+    storeFile(req, res, async (err) => {
+        if (err) {
+            res.status(400).json(err)
+            return
+        } else if (req.file == undefined) {
+            res.status(400).send("ERROR: No file selected.")
+            return
+        } else {
+            await FileManager.saveFile(req.file.filename)
+            res.json("SUCCESS: File uploaded successfully.")
+            return
+        }
+    })
 })
 
 module.exports = router;

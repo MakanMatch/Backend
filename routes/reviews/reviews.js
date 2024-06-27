e1ab025b8b40865057eb2034fc3c9871bda1d7fd
Lincoln Lim <lincolnlim267@gmail.com>
2024-06-27 21:00:13 +0800

    Added multer

--- routes/reviews/reviews.js
+++ routes/reviews/reviews.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const FileManager = require('../../services/FileManager');

9071f6e88c26c99fdfb1c92845a6d97bb4c3ff18
Lincoln Lim <lincolnlim267@gmail.com>
2024-06-28 09:10:09 +0800

    removed unnecessary and redundant where clause

--- routes/reviews/reviews.js
+++ routes/reviews/reviews.js
@@ -116,13 +116,12 @@ router.route("/reviews/")
             return res.send("SUCCESS: No fields to update");
         } else {
             try {
-                const updateReview = await Review.findByPk(req.query.id);
+                const updateReview = await Review.update(updateDict, {
+                    where: { reviewID: req.query.id }
+                });
                 if (!updateReview) {
                     return res.status(404).send(`UERROR: Review with ID ${req.query.id} not found`);
                 } else {
-                    await Review.update(updateDict, {
-                        where: { reviewID: req.query.id }
-                    })
                     res.send(`SUCCESS: Review with ID ${req.query.id} updated`); // Tested in postcode, working!
                 }
             } catch {
@@ -136,13 +135,12 @@ router.route("/reviews/")
             return res.status(400).send("ERROR: Missing review ID");
         }
         try {
-            const deleteReview = await Review.findByPk(req.query.id);
+            const deleteReview = await Review.destroy({
+                where: { reviewID: req.query.id }
+            });
             if (!deleteReview) {
                 return res.status(404).send(`ERROR: Review with ID ${req.query.id} not found`);
             } else {
-                await Review.destroy({
-                    where: { reviewID: req.query.id }
-                })
                 res.send(`SUCCESS: Review with ID ${req.query.id} deleted`); // Tested in postcode, working!
             }
         } catch {

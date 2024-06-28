89ea6e47fb4711b6e3dc8ec9e1b92814a816ecf4
Lincoln Lim <lincolnlim267@gmail.com>
2024-06-29 05:02:27 +0800

    Resolved  ERR_HTTP_HEADERS_SENT error.

--- routes/reviews/reviews.js
+++ routes/reviews/reviews.js
@@ -13,18 +13,31 @@ const Logger = require('../../services/Logger');
 router.route("/")
     .post(async (req, res) => {
         storeImages(req, res, async (err) => {
+
+            if (err instanceof multer.MulterError) {
+                Logger.log(`CDN REVIEWS POST ERROR: Image upload error; error: ${err}.`);
+                return res.status(400).send("ERROR: Image upload error");
+            } else if (err) {
+                Logger.log(`CDN REVIEWS POST ERROR: Internal server error; error: ${err}.`);
+                return res.status(500).send("ERROR: Internal server error");
+            }
+
             const hostID = Universal.data["DUMMY_HOST_ID"]
-            const host = await Host.findByPk(hostID)
             const guestID = Universal.data["DUMMY_GUEST_ID"]
-            const guest = await Guest.findByPk(guestID)
             const { foodRating, hygieneRating, comments, dateCreated } = req.body;
 
             if (!foodRating || !hygieneRating || !dateCreated) {
                 return res.status(400).send("ERROR: Missing required fields");
             }
             try {
-                const fileUrls = [];
+                // const host = await Host.findByPk(hostID)
+                // const guest = await Guest.findByPk(guestID)
+                // if (!host || !guest) {
+                //     return res.status(404).send("ERROR: Host or guest not found");
+                // }
+                // Not using the above code for now as we are hardcoding the hostID and guestID
 
+                const fileUrls = [];
                 for (const file of req.files) {
                     const saveResult = await FileManager.saveFile(file.filename);
                     if (saveResult !== true) {
@@ -53,14 +66,7 @@ router.route("/")
                 res.send("SUCCESS: Review submitted successfully");
             } catch {
                 Logger.log(`CDN REVIEWS POST ERROR: Failed to submit review; error: ${err}.`);
-                res.status(500).send("ERROR: Failed to submit review");
-            }
-            if (err instanceof multer.MulterError) {
-                Logger.log(`CDN REVIEWS POST ERROR: Image upload error; error: ${err}.`);
-                res.status(400).send("ERROR: Image upload error");
-            } else {
-                Logger.log(`CDN REVIEWS POST ERROR: Internal server error; error: ${err}.`);
-                res.status(500).send("ERROR: Internal server error");
+                return res.status(500).send("ERROR: Failed to submit review");
             }
         });
     });
@@ -104,9 +110,9 @@ router.route("/reviews/")
     .put(async (req, res) => {
         if (!req.query.id) {
             return res.status(400).send("ERROR: Missing review ID");
-            
+
         }
-        const updateFields = ["foodRating", "hygieneRating", "comments", "images", "likeCount", "dateCreated","guestID","hostID"];
+        const updateFields = ["foodRating", "hygieneRating", "comments", "images", "likeCount", "dateCreated", "guestID", "hostID"];
         const updateDict = {};
         updateFields.forEach(field => {
             if (req.query[field]) {

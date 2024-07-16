524e4f3908d4a9cfcd6689ba833cb207bdeca5ad
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-07-17 00:23:16 +0800

    added comments for improved code readability

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,16 +14,20 @@ const validateToken = (req, res, next) => {
         return res.status(401).send("ERROR: No authorization token found.");
     }
 
+    // Initialise a default TokenManager instance
     const tokenManager = TokenManager.default();
+
     var payload;
     var latestToken;
     var refreshed = false;
     try {
+        // Get TokenManager to verify the token and also refresh it if necessary
         const verification = tokenManager.verify(token, true);
         payload = verification.payload;
         latestToken = verification.token;
         refreshed = verification.refreshed;
     } catch (err) {
+        // Handle token verification/refreshing errors
         if (err.name == "TokenExpiredError") {
             return res.status(403).send("ERROR: Token expired. Please request new token.");
         } else {
@@ -32,13 +36,15 @@ const validateToken = (req, res, next) => {
         }
     }
 
+    // Populate request with user information
     req.user = payload;
     if (refreshed) {
-        // Inform client of new token so that they can replace
+        // Inform client of new token so that they can replace through RefreshedToken header
         Logger.log(`AUTH VALIDATETOKEN: Token refreshed for user with ID ${req.user.userID || "NOTFOUND"}.`);
         res.setHeader('RefreshedToken', latestToken); // THIS IS THE PROBLEM
     }
 
+    // Continue to next middleware
     next();
 };
 module.exports = { validateToken };

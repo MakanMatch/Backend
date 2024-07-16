36bc22b0a5ef71e7e1b41173dad58fd007ae5adb
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-07-16 19:24:38 +0800

    added createguest and createhost dbTools

--- dbTools.js
+++ dbTools.js
@@ -1,5 +1,7 @@
 const { v4: uuidv4 } = require('uuid')
-const { Admin, ChatHistory, ChatMessage, FoodListing, Guest, Host, Reservation, Review, Warning, sequelize } = require('./models')
+const prompt = require("prompt-sync")({ sigint: true });
+const { Admin, ChatHistory, ChatMessage, FoodListing, Guest, Host, Reservation, Review, Warning, sequelize } = require('./models');
+const Encryption = require('./services/Encryption');
 require('dotenv').config()
 
 async function resetDB() {
@@ -29,9 +31,94 @@ async function clearFiles() {
     console.log("Files cleared!")
 }
 
+async function createHost() {
+    var creating = true;
+    var createdHostIDs = []
+    while (creating) {
+        console.log("")
+        console.log("Creating a new host...")
+
+        const userID = uuidv4()
+        try {
+            const host = await Host.create({
+                userID: userID,
+                email: prompt("Email (must be unique): "),
+                password: await Encryption.hash(prompt("Password: ")),
+                username: prompt("Username (must be unique): "),
+                contactNum: prompt("Phone number (must be unique): "),
+                address: prompt("Address: "),
+                emailVerified: prompt("Email verified? (y/n): ").toLowerCase() !== 'n',
+                favCuisine: prompt("Favourite cuisine (optional): ") || null,
+                mealsMatched: parseInt(prompt("Meals matched (optional): ")) || 0,
+                foodRating: parseFloat(prompt("Food rating (optional): ")) || null,
+                hygieneGrade: parseFloat(prompt("Hygiene grade (optional): ")) || null,
+                paymentImage: prompt("Payment image (optional): ") || null
+            })
+        } catch (err) {
+            console.log("Failed to create host; error: " + err)
+            creating = prompt("Try again? (y/n) ") == "y"
+            console.log("")
+            continue
+        }
+
+        console.log("Host created!")
+        console.log(`Host ID: ${userID}`)
+        console.log("")
+        createdHostIDs.push(userID)
+
+        if (prompt("Create another host? (y/n): ").toLowerCase() !== 'y') {
+            creating = false;
+            console.log("")
+        }
+    }
+
+    console.log(createdHostIDs.length + " hosts created successfully.")
+}
+
+async function createGuest() {
+    var creating = true;
+    var createdGuestIDs = []
+    while (creating) {
+        console.log("")
+        console.log("Creating a new guest...")
+
+        const userID = uuidv4()
+        try {
+            const guest = await Guest.create({
+                userID: userID,
+                email: prompt("Email (must be unique): "),
+                password: await Encryption.hash(prompt("Password: ")),
+                username: prompt("Username (must be unique): "),
+                contactNum: prompt("Phone number (must be unique) (optional): ") || null,
+                address: prompt("Address (optional): ") || null,
+                emailVerified: prompt("Email verified? (y/n): ").toLowerCase() !== 'n',
+                favCuisine: prompt("Favourite cuisine (optional): ") || null,
+                mealsMatched: parseInt(prompt("Meals matched (optional): ")) || 0
+            })
+        } catch (err) {
+            console.log("Failed to create guest; error: " + err)
+            creating = prompt("Try again? (y/n) ") == "y"
+            console.log("")
+            continue
+        }
+
+        console.log("Guest created!")
+        console.log(`Guest ID: ${userID}`)
+        console.log("")
+        createdGuestIDs.push(userID)
+
+        if (prompt("Create another guest? (y/n): ").toLowerCase() !== 'y') {
+            creating = false;
+            console.log("")
+        }
+    }
+
+    console.log(createdGuestIDs.length + " guests created successfully.")
+}
+
 sequelize.sync({ alter: true })
-    .then(() => {
-        const tools = process.argv.slice(2)
+    .then(async () => {
+        const tools = (process.argv.slice(2)).map(t => t.toLowerCase())
         if (tools.length == 0) {
             console.log("No tool activated.")
             return
@@ -40,11 +127,19 @@ sequelize.sync({ alter: true })
         console.log()
 
         if (tools.includes("reset")) {
-            resetDB()
+            await resetDB()
         }
-        
+
         if (tools.includes("clearfiles")) {
-            clearFiles()
+            await clearFiles()
+        }
+
+        if (tools.includes("createhost")) {
+            await createHost()
+        }
+
+        if (tools.includes("createguest")) {
+            await createGuest()
         }
     })
     .catch(err => {

022742bc321f3050be3c471ba2327698a04b53e7
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-07-17 17:26:37 +0800

    new sanitiseData method to process objects into needed attributes only

--- services/Extensions.js
+++ services/Extensions.js
@@ -13,6 +13,30 @@ class Extensions {
     static timeDiffInSeconds(beforeDate, afterDate) {
         return (afterDate.getTime() - beforeDate.getTime()) / 1000;
     }
+
+    static sanitiseData(data, keys) {
+        var dataToReturn = {}
+        for (let attribute of Object.keys(data)) {
+            if (Array.isArray(data[attribute])) {
+                var sanitisedArray = []
+                for (let item of data[attribute]) {
+                    if (item instanceof Object) {
+                        sanitisedArray.push(Extensions.sanitiseData(item, keys))
+                    } else {
+                        sanitisedArray.push(item)
+                    }
+                }
+                dataToReturn[attribute] = sanitisedArray
+            } else if (data[attribute] instanceof Object) {
+                dataToReturn[attribute] = Extensions.sanitiseData(data[attribute], keys)
+            } else {
+                if (keys.includes(attribute)) {
+                    dataToReturn[attribute] = data[attribute]
+                }
+            }
+        }
+        return dataToReturn
+    }
 }
 
 module.exports = Extensions;

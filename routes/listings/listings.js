ee302d5079910e4945fd8cd3792697a642ad72fb
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2024-07-19 21:31:13 +0800

    Applied changes as suggested in PR re-request

--- routes/listings/listings.js
+++ routes/listings/listings.js
@@ -29,8 +29,10 @@ router.post("/addListing", validateToken, async (req, res) => {
             try {
                 const validatedData = await addListingSchema.validate(req.body, { abortEarly: false });
                 if (err instanceof multer.MulterError) {
+                    Logger.log(`LISTINGS ADDLISTING: Image upload error: ${err}`);
                     res.status(400).send("ERROR: Image upload error");
                 } else if (err) {
+                    Logger.log(`LISTINGS ADDLISTING: Internal server error: ${err}`);
                     res.status(500).send("ERROR: Internal server error");
                 } else {
                     var allImagesSuccess = false;
@@ -44,86 +46,86 @@ router.post("/addListing", validateToken, async (req, res) => {
                             break;
                         }
                     }
-                    if (allImagesSuccess === true) {
-                        const hostInfo = await Host.findByPk(req.user.userID);
-                        if (!hostInfo) {
-                            res.status(404).send("ERROR: Host not found");
-                            return;
-                        }
-                        try {
-                            const encodedAddress = encodeURIComponent(String(hostInfo.address));
-                            const apiKey = process.env.GMAPS_API_KEY;
-                            const url = `https://maps.googleapis.com/maps/api/geocode/json?address="${encodedAddress}"&key=${apiKey}`;
-                            const response = await axios.get(url);
-                            const location = response.data.results[0].geometry.location;
-                            const coordinates = { lat: location.lat, lng: location.lng };
-        
-                            const components = response.data.results[0].address_components;
-                            let street = '';
-                            let city = '';
-                            let state = '';
-        
-                            components.forEach(component => {
-                                if (component.types.includes('route')) {
-                                    street = component.long_name;
-                                }
-                                if (component.types.includes('locality')) {
-                                    city = component.long_name;
-                                }
-                                if (component.types.includes('administrative_area_level_1')) {
-                                    state = component.long_name;
-                                }
-                            });
-                            let approximateAddress = `${street}, ${city}`;
-                            if (state) {
-                                approximateAddress += `, ${state}`; // For contexts outside of Singapore
-                            }
-                            
-                            const formattedDatetime = validatedData.datetime + ":00.000Z";
-                            const listingDetails = {
-                                listingID: Universal.generateUniqueID(),
-                                title: validatedData.title,
-                                images: req.files.map(file => file.filename).join("|"),
-                                shortDescription: validatedData.shortDescription,
-                                longDescription: validatedData.longDescription,
-                                portionPrice: validatedData.portionPrice,
-                                totalSlots: validatedData.totalSlots,
-                                datetime: formattedDatetime,
-                                approxAddress: approximateAddress,
-                                address: hostInfo.address,
-                                hostID: req.user.userID,
-                                coordinates: coordinates.lat + "," + coordinates.lng,
-                                published: true,
-                            };
-                            const addListingResponse = await FoodListing.create(listingDetails);
-                            if (addListingResponse) {
-                                res.status(200).json({
-                                    message: "SUCCESS: Food listing created successfully",
-                                    listingDetails,
-                                });
-                                Logger.log(`LISTINGS ADDLISTING: Listing with listingID ${listingDetails.listingID} created successfully.`)
-                                return;
-                            } else {
-                                res.status(400).send("ERROR: Failed to create food listing");
-                                return;
-                            }
-                        } catch (error) {
-                            res.status(500).send("ERROR: Internal server error");
-                            Logger.log(`LISTINGS ADDLISTING: Internal server error: ${error}`);
-                            return;
-                        }
-                    } else {
+                    if (!allImagesSuccess) {
                         // Delete all images if one or more images failed to upload
                         for (let i=0; i<req.files.length; i++) {
                             await FileManager.deleteFile(req.files[i].filename);
-                            Logger.log(`LISTINGS ADDLISTING: One or more image checks failed. Image ${req.files[i].filename} deleted successfully.`)
                         }
+                        Logger.log(`LISTINGS ADDLISTING: One or more image checks failed. ${req.files.length} image(s) deleted successfully.`)
                         res.status(400).send("ERROR: Failed to upload image");
+                        return;
+                    }
+
+                    const hostInfo = await Host.findByPk(req.user.userID);
+                    if (!hostInfo) {
+                        res.status(404).send("ERROR: Host not found");
+                        return;
+                    }
+                    try {
+                        const encodedAddress = encodeURIComponent(String(hostInfo.address));
+                        const apiKey = process.env.GMAPS_API_KEY;
+                        const url = `https://maps.googleapis.com/maps/api/geocode/json?address="${encodedAddress}"&key=${apiKey}`;
+                        const response = await axios.get(url);
+                        const location = response.data.results[0].geometry.location;
+                        const coordinates = { lat: location.lat, lng: location.lng };
+    
+                        const components = response.data.results[0].address_components;
+                        let street = '';
+                        let city = '';
+                        let state = '';
+    
+                        components.forEach(component => {
+                            if (component.types.includes('route')) {
+                                street = component.long_name;
+                            }
+                            if (component.types.includes('locality')) {
+                                city = component.long_name;
+                            }
+                            if (component.types.includes('administrative_area_level_1')) {
+                                state = component.long_name;
+                            }
+                        });
+                        let approximateAddress = `${street}, ${city}`;
+                        if (state) {
+                            approximateAddress += `, ${state}`; // For contexts outside of Singapore
+                        }
+                        
+                        const formattedDatetime = validatedData.datetime + ":00.000Z";
+                        const listingDetails = {
+                            listingID: Universal.generateUniqueID(),
+                            title: validatedData.title,
+                            images: req.files.map(file => file.filename).join("|"),
+                            shortDescription: validatedData.shortDescription,
+                            longDescription: validatedData.longDescription,
+                            portionPrice: validatedData.portionPrice,
+                            totalSlots: validatedData.totalSlots,
+                            datetime: formattedDatetime,
+                            approxAddress: approximateAddress,
+                            address: hostInfo.address,
+                            hostID: req.user.userID,
+                            coordinates: coordinates.lat + "," + coordinates.lng,
+                            published: true,
+                        };
+                        const addListingResponse = await FoodListing.create(listingDetails);
+                        if (addListingResponse) {
+                            res.status(200).json({
+                                message: "SUCCESS: Food listing created successfully",
+                                listingDetails,
+                            });
+                            Logger.log(`LISTINGS ADDLISTING ERROR: Listing with listingID ${listingDetails.listingID} created successfully.`)
+                            return;
+                        } else {
+                            res.status(400).send("ERROR: Failed to create food listing");
+                            return;
+                        }
+                    } catch (error) {
+                        res.status(500).send("ERROR: Internal server error");
+                        Logger.log(`LISTINGS ADDLISTING ERROR: Internal server error: ${error}`);
                         return;
                     }
                 }
             } catch (validationError) {
-                res.status(400).send(`Validation Error: ${validationError.errors.join(', ')}`);
+                res.status(400).send(`ERROR: ${validationError.errors.join(', ')}`);
                 return;
             }
         }

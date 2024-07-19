25c2ba9310a78a0e06da7aedeb3db77f0d632e01
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-07-19 10:25:18 +0800

    improved sanitiseData

--- services/Extensions.js
+++ services/Extensions.js
@@ -14,25 +14,35 @@ class Extensions {
         return (afterDate.getTime() - beforeDate.getTime()) / 1000;
     }
 
-    static sanitiseData(data, allowedKeys, allowedTopLevelKeys=[]) {
+    static sanitiseData(data, allowedKeys=[], disallowedKeys=[], allowedTopLevelKeys=[]) {
+        if (allowedKeys.length == 0 && disallowedKeys.length == 0 && allowedTopLevelKeys.length == 0) { return data }
         var dataToReturn = {}
         for (let attribute of Object.keys(data)) {
             if (allowedTopLevelKeys.includes(attribute)) {
+                // Key is an explicitly allowed top-level key
                 dataToReturn[attribute] = data[attribute]
+            } else if (disallowedKeys.includes(attribute)) {
+                // Key is an explicitly disallowed attribute
+                continue
             } else if (Array.isArray(data[attribute])) {
+                // Key has an array as a value
                 var sanitisedArray = []
                 for (let item of data[attribute]) {
                     if (item instanceof Object) {
-                        sanitisedArray.push(Extensions.sanitiseData(item, allowedKeys, allowedTopLevelKeys))
+                        // Array item is an dictionary
+                        sanitisedArray.push(Extensions.sanitiseData(item, allowedKeys, disallowedKeys, allowedTopLevelKeys))
                     } else {
+                        // Array item is a regular item
                         sanitisedArray.push(item)
                     }
                 }
                 dataToReturn[attribute] = sanitisedArray
             } else if (data[attribute] instanceof Object) {
-                dataToReturn[attribute] = Extensions.sanitiseData(data[attribute], allowedKeys, allowedTopLevelKeys)
+                // Key has a dictionary as a value
+                dataToReturn[attribute] = Extensions.sanitiseData(data[attribute], allowedKeys, disallowedKeys, allowedTopLevelKeys)
             } else {
-                if (allowedKeys.includes(attribute)) {
+                if (allowedKeys.length == 0 || allowedKeys.includes(attribute)) {
+                    // Key is either explicitly allowed or was not explicitly disallowed
                     dataToReturn[attribute] = data[attribute]
                 }
             }

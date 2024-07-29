6f10c0b2cfd4d794cd59ddde6117ae83a6898fe2
Lincoln Lim <lincolnlim267@gmail.com>
2024-07-29 18:08:21 +0800

    uploadPaymentQR route endpoint for payment image uploading

--- routes/orders/uploadPaymentQR.js
+++ routes/orders/uploadPaymentQR.js
@@ -5,6 +5,8 @@ const path = require('path')
 const { FoodListing, Review, Host } = require("../../models");
 const { storeFile } = require("../../middleware/storeFile");
 const { validateToken } = require("../../middleware/auth");
+const Logger = require("../../services/Logger");
+const multer = require("multer");
 
 router.route("/")
     .post(validateToken, async (req, res) => {
@@ -16,36 +18,56 @@ router.route("/")
                 Logger.log(`REVIEWS SUBMITREVIEW POST ERROR: Internal server error; error: ${err}.`);
                 return res.status(500).send("ERROR: Internal server error");
             }
-        })
 
-        const { listingID, hostID } = req.body;
+            const hostID = req.user.userID;
 
-        if (!listingID || !hostID) {
-            return res.status(400).send("ERROR: Missing required fields");
-        }
-
-        if(!req.file) {
-            return res.status(400).send("ERROR: No file uploaded");
-        }
-
-        try {
-            const findListing = await FoodListing.findByPk(listingID);
-            const findHost = await Host.findByPk(hostID);
-
-            if (!findListing || !findHost) {
-                return res.status(404).send("ERROR: Listing or host not found.");
+            if (!hostID) {
+                return res.status(400).send("ERROR: Missing required fields");
             }
 
-            const saveFile = await FileManager.saveFile(req.file.filename);
-            if (saveFile == true) {
-                return res.status(200).send("SUCCESS: File uploaded");
-            } else {
+            console.log(req.body)
+            console.log(req.file)
+            try {
+                var host = await Host.findByPk(hostID);
+                if (!host) {
+                    return res.status(404).send("ERROR: Listing or host not found.");
+                }
+
+                if (req.file) {
+                    try {
+                        const saveFile = await FileManager.saveFile(req.file.filename);
+                        if (!saveFile) {
+                            Logger.log(`REVIEWS SUBMITREVIEW POST ERROR: Failed to save file.`);
+                            return res.status(500).send("ERROR: Failed to save file");
+                        }
+                    } catch (err) {
+                        Logger.log(`REVIEWS SUBMITREVIEW POST ERROR: Failed to upload file; error: ${err}.`);
+                        return res.status(500).send("ERROR: Failed to upload file");
+                    }
+                }
+            } catch (err) {
+                Logger.log(`ORDERS UPLOADPAYMENT QR POST ERROR: Failed to upload file; error: ${err}.`);
                 return res.status(500).send("ERROR: Failed to upload file");
             }
-        } catch (err) {
-            Logger.log(`ORDERS UPLOADPAYMENT QR POST ERROR: Failed to upload file; error: ${err}.`);
-            return res.status(500).send("ERROR: Failed to upload file");
-        }
 
-        // Update Host Payment Image
+            // Update Host Payment Image
+            try {
+                host.set({
+                    paymentImage: req.file.filename
+                });
+
+                const updateHostPaymentImage = await host.save();
+                
+                if (!updateHostPaymentImage) {
+                    return res.status(500).send("ERROR: Failed to update Host Payment Image");
+                }
+
+                return res.status(200).send("SUCCESS: Payment QR uploaded successfully");
+            } catch (err) {
+                Logger.log(`ORDERS UPLOADPAYMENT QR POST ERROR: Failed to upload Payment QR; error: ${err}.`);
+                return res.status(500).send("ERROR: Failed to upload Payment QR")
+            }
+        })
     })
+
+module.exports = { router, at: '/orders/uploadPaymentQR' };

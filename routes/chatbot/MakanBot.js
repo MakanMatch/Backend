4dbd9b10d66fd525a2e340d561e82ee7d7330885
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2024-08-01 10:35:32 +0800

    Added error handling

--- routes/chatbot/MakanBot.js
+++ routes/chatbot/MakanBot.js
@@ -1,34 +1,27 @@
 const express = require("express");
-const multer = require("multer");
 const router = express.Router();
-const { FoodListing } = require("../../models");
-const { Host, Guest, UserRecord, FavouriteListing } = require("../../models");
-const Universal = require("../../services/Universal");
-const FileManager = require("../../services/FileManager");
 const Logger = require("../../services/Logger")
-const { storeImages } = require("../../middleware/storeImages");
-const axios = require("axios");
-const yup = require("yup");
-const { validateToken } = require("../../middleware/auth");
-const { Op } = require("sequelize");
 const OpenAIChat = require("../../services/OpenAIChat");
 
 router.post("/queryMakanBotWithUserPrompt", async (req, res) => {
     const { messagePrompt, conversationHistory } = req.body;
 
-    console.log("Message Prompt: ", messagePrompt);
-    console.log("Conversation History: ", conversationHistory);
+    if (!OpenAIChat.checkPermission()) {
+        return res.status(400).send("ERROR: OpenAIChat is not available at this time.");
+    }
 
-    (async () => {
-        const message = await OpenAIChat.prompt(
-            messagePrompt,
-            true,
-            conversationHistory
-        )
-        res.status(200).json({ message: message.content });
-    })();
+    const message = await OpenAIChat.prompt(
+        messagePrompt,
+        true,
+        conversationHistory
+    )
+
+    if (typeof message === "string" && message.startsWith("ERROR:")) {
+        Logger.log(`CHATBOT MAKANBOT QUERYMAKANBOTWITHUSERPROMPT ERROR: Failed to run prompt through OpenAIChat; response: ${message}`)
+        return res.status(400).send("ERROR: Failed to run prompt. Try again later.");
+    } else {
+        return res.status(200).json({ message: message.content });
+    }
 });
-
-
 
 module.exports = { router, at: '/makanBot' };
--- services/OpenAIChat.js
+++ services/OpenAIChat.js
@@ -74,7 +74,7 @@ class OpenAIChat {
     }
 
     static checkPermission() {
-        return process.env.OPENAI_API_KEY && process.env.OPENAI_CHAT_ENABLED === 'True';
+        return process.env.OPENAI_API_KEY !== undefined && process.env.OPENAI_CHAT_ENABLED === 'True';
     }
 
     static initialise(configOptions={ model: "gpt-4o-mini", maxTokens: 512, temperature: 0.5 }) {

ec81943ebda7d7cd6aadea073262d3a6c4b01143
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2024-08-07 22:03:18 +0800

    Added backend API for retrieving the listings of a host

---
+++ routes/listings/getHostListings.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const FileManager = require("../../services/FileManager");
+const Extensions = require("../../services/Extensions");
+const { FoodListing, Host, Guest, Admin, Review, Reservation, ReviewLike, UserRecord } = require("../../models");
+const Logger = require("../../services/Logger");
+const { Sequelize } = require('sequelize');
+const Universal = require("../../services/Universal");
+const { validateToken, checkUser } = require("../../middleware/auth");
+const { Op } = require("sequelize");
+
+router.get("/", validateToken, async (req, res) => { // GET all hosts' food listings
+    const hostID = req.user.userID;
+    const hostListings = await FoodListing.findAll({
+        where: {
+            hostID: hostID
+        },
+        include: [
+            {
+                model: Host,
+                as: 'Host'
+            }
+        ]
+    });
+    return res.status(200).send(hostListings);
+});
+
+module.exports = { router, at: '/listings/getHostListings' };

a236681b91c51080b6f4b744e38d843eb9b2e916
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-08-09 18:04:52 +0800

    fixed getHostListings

--- routes/listings/getHostListings.js
+++ routes/listings/getHostListings.js
@@ -1,23 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
-const FileManager = require("../../services/FileManager");
-const Extensions = require("../../services/Extensions");
-const { FoodListing, Host, Guest, Admin, Review, Reservation, ReviewLike, UserRecord } = require("../../models");
-const Logger = require("../../services/Logger");
-const { Sequelize } = require('sequelize');
-const Universal = require("../../services/Universal");
-const { validateToken, checkUser } = require("../../middleware/auth");
-const { Op } = require("sequelize");
+const { FoodListing } = require("../../models");
+const { validateToken } = require("../../middleware/auth");
+const { Logger } = require("../../services");
 
 router.get("/", validateToken, async (req, res) => { // GET all hosts' food listings
     const hostID = req.user.userID;
-    const hostListings = await FoodListing.findAll({
-        where: {
-            hostID: hostID
-        }
-    });
-    return res.status(200).send(hostListings);
+    if (req.user.userType !== "Host") {
+        return res.status(400).send("UERROR: You need to be a host before you can view your listings.");
+    }
+
+    try {
+        const hostListings = await FoodListing.findAll({
+            where: {
+                hostID: hostID
+            }
+        });
+        
+        return res.status(200).send(hostListings);
+    } catch (err) {
+        Logger.log(`LISTINGS GETHOSTLISTINGS ERROR: ${err}`);
+        return res.status(500).send("ERROR: Failed to retrieve host listings.");
+    }
 });
 
 module.exports = { router, at: '/listings/getHostListings' };

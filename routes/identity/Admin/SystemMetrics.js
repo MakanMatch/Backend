14d27eeb265f30f0e256c43338e1fa3268d9d26a
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2024-08-11 18:46:58 +0800

    new /getMetrics endpoint for admins to see metadata analytics for the system

---
+++ routes/identity/Admin/SystemMetrics.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const { Analytics, Logger } = require('../../../services');
+const { validateAdmin } = require('../../../middleware/auth');
+
+
+router.get("/getMetrics", validateAdmin, async (req, res) => {
+    if (!Analytics.checkPermission()) {
+        res.status(400).send("ERROR: Analytics are not available at this time.")
+        return;
+    }
+
+    var allData = {};
+    try {
+        var systemMetrics = await Analytics.getSystemMetrics();
+        if (systemMetrics.instanceID) {
+            delete systemMetrics.instanceID;
+        }
+        if (systemMetrics.createdAt) {
+            delete systemMetrics.createdAt;
+        }
+        if (systemMetrics.updatedAt) {
+            delete systemMetrics.updatedAt
+        }
+
+        var requestMetrics = await Analytics.getRequestMetrics();
+        requestMetrics = requestMetrics.map(request => {
+            if (request.createdAt) {
+                delete request.createdAt
+            }
+            if (request.updatedAt) {
+                delete request.updatedAt
+            }
+
+            return request;
+        })
+        
+        allData.systemAnalytics = systemMetrics;
+        allData.requestAnalytics = requestMetrics;
+    } catch (err) {
+        Logger.log(`IDENTITY ADMIN SYSTEMMETRICS ERROR: Failed to retrieve system and request metrics for admin; error: ${err}`)
+        res.status(500).send("ERROR: Failed to process request.")
+        return;
+    }
+
+    return res.json(allData);
+})
+
+module.exports = { router, at: "/admin" }
